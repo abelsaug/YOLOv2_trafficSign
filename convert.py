0bfc999081864238c3cce4206a147349c77a9927
abelsaug <asa.11ec05@nitk.edu.in>
2018-01-18 23:17:09 -0600

    Changes

--- convert.py
+++ convert.py
@@ -7,12 +7,14 @@ This script is to convert the txt annotation files to appropriate format needed 
 @author: Guanghan Ning
 Email: gnxr9@mail.missouri.edu
 """
+"""
+Modifications for GTSDB by @author Abel Augustine
+"""
 
 import os
 from os import walk, getcwd
 from PIL import Image
 
-classes = ["stopsign"]
 
 def convert(size, box):
     dw = 1./size[0]
@@ -26,79 +28,44 @@ def convert(size, box):
     y = y*dh
     h = h*dh
     return (x,y,w,h)
-    
-    
-"""-------------------------------------------------------------------""" 
 
-""" Configure Paths"""   
-mypath = "labels/stopsign_original/"
-outpath = "labels/stopsign/"
 
-cls = "stopsign"
-if cls not in classes:
-    exit(0)
-cls_id = classes.index(cls)
+# """ Configure Paths"""   
+outpath = "FullIJCNN2013/"
 
-wd = getcwd()
-list_file = open('%s/%s_list.txt'%(wd, cls), 'w')
-
-""" Get input text file list """
-txt_name_list = []
-for (dirpath, dirnames, filenames) in walk(mypath):
-    txt_name_list.extend(filenames)
-    break
-print(txt_name_list)
-
-""" Process """
-for txt_name in txt_name_list:
-    # txt_file =  open("Labels/stop_sign/001.txt", "r")
-    
-    """ Open input text files """
-    txt_path = mypath + txt_name
-    print("Input:" + txt_path)
-    txt_file = open(txt_path, "r")
-    lines = txt_file.read().split('\r\n')   #for ubuntu, use "\r\n" instead of "\n"
-    
-    """ Open output text files """
-    txt_outpath = outpath + txt_name
+txt_file =  open("FullIJCNN2013/gt.txt", "r")
+lines = txt_file.read().split('\n')
+ct = 0
+print len(lines)
+for line in lines:
+# """ Open output text files """
+    lineParts = line.split(';')
+    if lineParts[0] == "":
+        continue
+    print lineParts
+    if int(lineParts[5])>9:
+        txt_outpath = outpath +  lineParts[0].strip(".ppm")+".txt"
+    else:
+        txt_outpath = outpath + lineParts[0].strip(".ppm")+".txt"
     print("Output:" + txt_outpath)
-    txt_outfile = open(txt_outpath, "w")
-    
-    
-    """ Convert the data to YOLO format """
-    ct = 0
-    for line in lines:
-        #print('lenth of line is: ')
-        #print(len(line))
-        #print('\n')
-        if(len(line) >= 2):
-            ct = ct + 1
-            print(line + "\n")
-            elems = line.split(' ')
-            print(elems)
-            xmin = elems[0]
-            xmax = elems[2]
-            ymin = elems[1]
-            ymax = elems[3]
-            #
-            img_path = str('%s/images/%s/%s.JPEG'%(wd, cls, os.path.splitext(txt_name)[0]))
-            #t = magic.from_file(img_path)
-            #wh= re.search('(\d+) x (\d+)', t).groups()
-            im=Image.open(img_path)
-            w= int(im.size[0])
-            h= int(im.size[1])
-            #w = int(xmax) - int(xmin)
-            #h = int(ymax) - int(ymin)
-            # print(xmin)
-            print(w, h)
-            b = (float(xmin), float(xmax), float(ymin), float(ymax))
-            bb = convert((w,h), b)
-            print(bb)
-            txt_outfile.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
+    txt_outfile = open(txt_outpath, "a")
 
-    """ Save those images with bb into list"""
-    if(ct != 0):
-        list_file.write('%s/images/%s/%s.JPEG\n'%(wd, cls, os.path.splitext(txt_name)[0]))
-                
-list_file.close()       
+    cls_id = lineParts[5]
+# """ Convert the data to YOLO format """
+    ct = ct + 1
+    elems = lineParts
+    xmin = elems[1]
+    ymin = elems[2]
+    xmax = elems[3]
+    ymax = elems[4]
 
+    img_path = str('FullIJCNN2013/%s'%(elems[0]))
+    im=Image.open(img_path)
+    w= int(im.size[0])
+    h= int(im.size[1])
+    b = (float(xmin), float(xmax), float(ymin), float(ymax))
+    bb = convert((w,h), b)
+
+    print(bb)
+    # im.save('FullIJCNN2013/%s'%(elems[0].strip(".ppm")+".JPEG"))
+    txt_outfile.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
